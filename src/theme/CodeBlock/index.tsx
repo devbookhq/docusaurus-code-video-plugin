f6a28caccb488b81a37dd9b33b39086af68fd9dd
Tomas Valenta <valenta.and.thomas@gmail.com>
2022-07-17 10:32:22 +0000

    Add tsc css plugin

--- src/theme/CodeBlock/index.tsx
+++ src/theme/CodeBlock/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import CodeBlock, { type Props } from '@theme-init/CodeBlock';
 
+import './index.css'
+
 export default function CodeBlockWrapper(props: Props) {
   return (
     <>
